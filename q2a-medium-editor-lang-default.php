535981a8e15d457a45b13b85aaa375ed0470a373
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-09-29 10:25:31 +0900

    Add language file

---
+++ q2a-medium-editor-lang-default.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+    Plugin Name: Medium Editor
+*/
+
+return array(
+    // default Japanese
+    'default_height' => '規定のエディタの高さ: ',
+    'upload_images' => '画像のアップロードを許可',
+    'upload_max_size' => '最大アップロードファイルサイズ:',
+    'upload_max_img_width' => '最大アップロード画像横幅:',
+    'place_holder' => '詳細を記載してください。画像も投稿できます。(＋)ボタンを押してください。\nすでにたくさんの質問がありますが、質問の重複を気にせずに、気楽に投稿してください。',
+);
+
+/*
+    Omit PHP closing tag to help avoid accidental output
+*/
