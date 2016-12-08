a0313a4ebc2537ef320de65c4eb8c30c1a1b07eb
yshiga <yuichi.shiga@gmail.com>
2016-12-09 06:10:10 +0900

    フォームごとに高さを設定した

--- js/q2a-editor.js
+++ js/q2a-editor.js
@@ -1,27 +1,9 @@
 $(function() {
   $('.qa-part-form .editable').focus(function(){
-    var height = $(this).height();
-    if (height <= 550) {
-      $(this).animate({
-        height: '550px',
-      }, 'slow' );
-    }
   });
 
   $('.qa-a-form .editable').focus(function(){
-    var height = $(this).height();
-    if (height <= 550) {
-      $(this).animate({
-        height: '550px',
-      }, 'slow' );
-    }
   });
   $('.qa-c-form .editable').focus(function(){
-    var height = $(this).height();
-    if (height <= 600) {
-      $(this).animate({
-        height: '600px',
-      }, 'slow' );
-    }
   });
 });
