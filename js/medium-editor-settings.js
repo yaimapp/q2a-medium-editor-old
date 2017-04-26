9e7d83b1f7254e34b96588149b5922bd2a514816
yshiga <yuichi.shiga@gmail.com>
2017-04-26 23:29:08 +0900

    jsファイルのリファクタ

--- js/medium-editor-settings.js
+++ js/medium-editor-settings.js
@@ -36,19 +36,3 @@ $(function() {
         },
     });
 });
-
-function get_content(name) {
-    var editor_elm = document.getElementsByName(name);
-
-    if (editor_elm.length > 0) {
-        var target = MediumEditor.getEditorFromElement(editor_elm[0]);
-        var allContents = target.serialize();
-        var editorId = target.elements[0].id;
-        var content = allContents[editorId].value;
-        content = content.replace(/<div class=\"video video-youtube\">.*?<\/div>/g, '');
-        content = content.replace(/medium-insert-embeds-selected/g, '');
-    } else {
-        content = '';
-    }
-    return content;
-}
