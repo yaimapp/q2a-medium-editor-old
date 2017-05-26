c3796674e9a37726a2c96a56a7c64e984ac0d533
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-05-26 11:28:33 +0900

    保存のときプログレスバー削除

--- q2a-medium-editor.php
+++ q2a-medium-editor.php
@@ -104,6 +104,7 @@ class qa_medium_editor
         $html = '';
         $placeholder = '';
 
+        $content = $this->remove_progressbar($content);
         $content = $this->embed_replace($content);
         if (empty($format)) {
             $content = nl2br($content);
@@ -194,6 +195,17 @@ class qa_medium_editor
             'format' => 'html',
             'content' => qa_sanitize_html($html, false, true),
         );
+    }
+
+    /*
+     * プログレスバーが残っている場合に削除する
+     */
+    private function remove_progressbar($content)
+    {
+        $regex = "/\<div\s?class=\"[^\"]*bar[^\"]*\"[^>]*><\/div>/Us";
+        $regex2 = "/\<div\s?class=\"mdl-progress\s?[^\"]*\"[^>]*><\/div>/Us";
+        $tmp = preg_replace($regex, "", $content);
+        return preg_replace($regex2, "", $tmp);
     }
 }
 
